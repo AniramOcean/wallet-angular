b139b7e38af7cac16c625da75a9d37d3f1c6c962
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-08-12 19:09:11 +0300

    feat: add templates, styles, ngIf, ngFor, one&two-way-binding, addPurchase, updateSum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WalletModule } from './components/wallet/wallet.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    WalletModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/components/wallet/wallet.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WalletComponent } from './wallet.component';
+import { PurchasePreviewModule } from './purchase-preview/purchase-preview.module';
+import { PurchaseAddModule } from './purchase-add/purchase-add.module';
+
+
+
+@NgModule({
+  declarations: [
+    WalletComponent
+  ],
+  exports: [
+    WalletComponent
+  ],
+  imports: [
+    CommonModule,
+    PurchasePreviewModule,
+    PurchaseAddModule
+  ]
+})
+export class WalletModule { }
